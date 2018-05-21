e9c587206d16efb40ecbd111ba8cb4cc82d066fa
Adilf <Adilf@users.noreply.github.com>
2018-05-21 20:34:24 +0300

    Fixed tabs\whitespaces

--- Source/InterceptRest.cs
+++ Source/InterceptRest.cs
@@ -11,150 +11,149 @@ using Harmony;
 
 namespace UseBedrolls
 {
-    [HarmonyPatch(typeof(JobGiver_GetRest), "TryGiveJob")]
-    static class InterceptRest
-    {
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            MethodInfo FindBedrollJobInfo = AccessTools.Method(typeof(InterceptRest), nameof(InterceptRest.FindBedrollJob));
+	[HarmonyPatch(typeof(JobGiver_GetRest), "TryGiveJob")]
+	static class InterceptRest
+	{
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo FindBedrollJobInfo = AccessTools.Method(typeof(InterceptRest), nameof(InterceptRest.FindBedrollJob));
 
-            bool foundNew = false;
-            foreach (CodeInstruction i in instructions)
-            {
-                //ldsfld       class Verse.JobDef RimWorld.JobDefOf::LayDown
-                yield return i;
-                if (i.opcode == OpCodes.Newobj)
-                {
-                    if (!foundNew) foundNew = true;
-                    else
-                    {
-                        yield return new CodeInstruction(OpCodes.Ldarg_1);
-                        yield return new CodeInstruction(OpCodes.Call, FindBedrollJobInfo);
-                    }
-                }
-            }
-        }
+			bool foundNew = false;
+			foreach(CodeInstruction i in instructions)
+			{
+				//ldsfld	   class Verse.JobDef RimWorld.JobDefOf::LayDown
+				yield return i;
+				if (i.opcode == OpCodes.Newobj)
+				{
+					if (!foundNew) foundNew = true;
+					else
+					{
+						yield return new CodeInstruction(OpCodes.Ldarg_1);
+						yield return new CodeInstruction(OpCodes.Call, FindBedrollJobInfo);
+					}
+				}
+			}
+		}
 
-        static Job FindBedrollJob(Job fallbackJob, Pawn pawn)
-        {
-            if (!pawn.IsColonistPlayerControlled) return fallbackJob;
-            Log.Message(pawn + " looking for inventory beds");
+		static Job FindBedrollJob(Job fallbackJob, Pawn pawn)
+		{	
+			if (!pawn.IsColonistPlayerControlled) return fallbackJob;
+			Log.Message(pawn + " looking for inventory beds");
 
-            Thing spare_bed = pawn.inventory.innerContainer.FirstOrDefault(tmini => tmini.GetInnerIfMinified() is Building_Bed b && b.def.building.bed_humanlike);
-            if (spare_bed == null)
-            {
-                spare_bed = nearest_bed_laying_around(pawn);
-                if (spare_bed == null)
-                {
-                    Pawn nearby_pawn = who_can_share_with(pawn);
-                    if ((nearby_pawn != null))
-                    {
-                        spare_bed = nearby_pawn.inventory.innerContainer.FirstOrDefault(tmini => tmini.GetInnerIfMinified() is Building_Bed b && b.def.building.bed_humanlike);
-                        nearby_pawn.inventory.innerContainer.TryDrop(spare_bed, ThingPlaceMode.Near, out spare_bed);
-                        Log.Message(nearby_pawn.Name + " dropped bed at " + spare_bed.Position);
-                    }
-                }
-            };
-            MinifiedThing invBed = (MinifiedThing)spare_bed;
-            if (invBed == null)
-                return fallbackJob;
-            Log.Message(pawn + " found " + invBed);
+			Thing spare_bed = pawn.inventory.innerContainer.FirstOrDefault(tmini => tmini.GetInnerIfMinified() is Building_Bed b && b.def.building.bed_humanlike);
+			if (spare_bed == null)
+			{
+				spare_bed = nearest_bed_laying_around(pawn);
+				if (spare_bed == null)
+				{
+					Pawn nearby_pawn = who_can_share_with(pawn);
+					if ((nearby_pawn != null))
+					{
+						spare_bed = nearby_pawn.inventory.innerContainer.FirstOrDefault(tmini => tmini.GetInnerIfMinified() is Building_Bed b && b.def.building.bed_humanlike);
+						nearby_pawn.inventory.innerContainer.TryDrop(spare_bed, ThingPlaceMode.Near,out spare_bed);
+						Log.Message(nearby_pawn.Name + " dropped bed at " + spare_bed.Position);
+					}
+				}
+			};
+			MinifiedThing invBed = (MinifiedThing)spare_bed;
+			if (invBed==null)
+				return fallbackJob;
+			Log.Message(pawn + " found " + invBed);
 
-            Map map = pawn.Map;
-            Building_Bed bed = (Building_Bed)invBed.GetInnerIfMinified();
+			Map map = pawn.Map;
+			Building_Bed bed = (Building_Bed)invBed.GetInnerIfMinified();
 
-            Func<IntVec3, Rot4, bool> cellValidatorDir = delegate (IntVec3 c, Rot4 direction)
-            {
-                if (RegionAndRoomQuery.RoomAtFast(c, map).isPrisonCell != pawn.IsPrisoner)
-                    return false;
+			Func<IntVec3, Rot4, bool> cellValidatorDir = delegate (IntVec3 c, Rot4 direction)
+			{
+				if (RegionAndRoomQuery.RoomAtFast(c,map).isPrisonCell != pawn.IsPrisoner)
+					return false;
 
-                if (!GenConstruct.CanPlaceBlueprintAt(invBed.GetInnerIfMinified().def, c, direction, map).Accepted)
-                    return false;
+				if (!GenConstruct.CanPlaceBlueprintAt(invBed.GetInnerIfMinified().def, c, direction, map).Accepted)
+					return false;
 
-                for (CellRect.CellRectIterator iterator = GenAdj.OccupiedRect(c, direction, bed.def.size).GetIterator();
-                        !iterator.Done(); iterator.MoveNext())
-                    foreach (Thing t in iterator.Current.GetThingList(map))
-                        if (!(t is Pawn) && GenConstruct.BlocksConstruction(bed, t))
-                            return false;
+				for (CellRect.CellRectIterator iterator = GenAdj.OccupiedRect(c, direction, bed.def.size).GetIterator();
+						!iterator.Done(); iterator.MoveNext())
+					foreach (Thing t in iterator.Current.GetThingList(map))
+						if (!(t is Pawn) && GenConstruct.BlocksConstruction(bed, t))
+							return false;
 
-                return true;
-            };
+				return true;
+			};
 
-            // North/East would be redundant, except for cells on edge ; oh well, too much code to handle that
-            Predicate<IntVec3> cellValidator = c => cellValidatorDir(c, Rot4.South) || cellValidatorDir(c, Rot4.West);
+			// North/East would be redundant, except for cells on edge ; oh well, too much code to handle that
+			Predicate<IntVec3> cellValidator = c => cellValidatorDir(c, Rot4.South) || cellValidatorDir(c, Rot4.West);
+			
+			Predicate<IntVec3> goodCellValidator = c =>
+				!RegionAndRoomQuery.RoomAt(c, map).PsychologicallyOutdoors && cellValidator(c);
 
-            Predicate<IntVec3> goodCellValidator = c =>
-                !RegionAndRoomQuery.RoomAt(c, map).PsychologicallyOutdoors && cellValidator(c);
+			IntVec3 placePosition = IntVec3.Invalid;
+			IntVec3 root = pawn.Position;
+			TraverseParms trav = TraverseParms.For(pawn);
+			if (!CellFinder.TryFindRandomReachableCellNear(root, map, 4, trav, goodCellValidator, null, out placePosition))
+				if (!CellFinder.TryFindRandomReachableCellNear(root, map, 12, trav, goodCellValidator, null, out placePosition))
+					if (!CellFinder.TryFindRandomReachableCellNear(root, map, 4, trav, cellValidator, null, out placePosition))
+						CellFinder.TryFindRandomReachableCellNear(root, map, 12, trav, cellValidator, null, out placePosition);
 
-            IntVec3 placePosition = IntVec3.Invalid;
-            IntVec3 root = pawn.Position;
-            TraverseParms trav = TraverseParms.For(pawn);
-            if (!CellFinder.TryFindRandomReachableCellNear(root, map, 4, trav, goodCellValidator, null, out placePosition))
-                if (!CellFinder.TryFindRandomReachableCellNear(root, map, 12, trav, goodCellValidator, null, out placePosition))
-                    if (!CellFinder.TryFindRandomReachableCellNear(root, map, 4, trav, cellValidator, null, out placePosition))
-                        CellFinder.TryFindRandomReachableCellNear(root, map, 12, trav, cellValidator, null, out placePosition);
+			if (placePosition.IsValid)
+			{
+				Rot4 dir = cellValidatorDir(placePosition, Rot4.South) ? Rot4.South : Rot4.West;
+				Blueprint_Install blueprint = GenConstruct.PlaceBlueprintForInstall(invBed, placePosition, map, dir, pawn.Faction);
 
-            if (placePosition.IsValid)
-            {
-                Rot4 dir = cellValidatorDir(placePosition, Rot4.South) ? Rot4.South : Rot4.West;
-                Blueprint_Install blueprint = GenConstruct.PlaceBlueprintForInstall(invBed, placePosition, map, dir, pawn.Faction);
+				Log.Message(pawn + " placing " + blueprint + " at " + placePosition);
 
-                Log.Message(pawn + " placing " + blueprint + " at " + placePosition);
+				return new Job(JobDefOf.PlaceBedroll, invBed, blueprint)
+				{
+					haulMode = HaulMode.ToContainer
+				};
+			}
+			Log.Message(pawn + " couldn't find place for " + invBed);
 
-                return new Job(JobDefOf.PlaceBedroll, invBed, blueprint)
-                {
-                    haulMode = HaulMode.ToContainer
-                };
-            }
-            Log.Message(pawn + " couldn't find place for " + invBed);
+			return fallbackJob;
+		}
 
-            return fallbackJob;
-        }
+		private static Pawn who_can_share_with(Pawn sleepy_pawn)
+		{
+			List<Pawn> pawns = sleepy_pawn.Map.mapPawns.SpawnedPawnsInFaction(sleepy_pawn.Faction).ListFullCopy();
+			Log.Message("pawns are " + pawns.ToStringSafeEnumerable());
+			TraverseParms traverseParams = TraverseParms.For(sleepy_pawn, Danger.Deadly, TraverseMode.ByPawn, false);
+			Predicate<Pawn> surplusFinder = delegate (Pawn p) {
+				int count = p.CountBeds();
+				Log.Message(p.Name + " has " + count + " beds");
+				if (((p.RaceProps.Animal || p.ownership.OwnedBed != null) && count > 0)
+					|| (count > 1))
+				{
+					Log.Message(p.Name + " has can spare some");
+					if (sleepy_pawn.Map.reachability.CanReach(sleepy_pawn.Position, p, PathEndMode.ClosestTouch, traverseParams))
+					{
+						Log.Message(sleepy_pawn.Name + " can reach " + p.Name);
+						return true;
+					}
+				}
+				return false;
+			};
+			List<Pawn> surplusPawns = pawns.FindAll(surplusFinder);
+			if (surplusPawns.NullOrEmpty())
+				return null;
+			Pawn generous_pawn = surplusPawns.MinBy(p => DistanceTo(p,sleepy_pawn));
+			Log.Message("surplusPawns are " + generous_pawn);
+			return generous_pawn;
+		}
 
-        private static Pawn who_can_share_with(Pawn sleepy_pawn)
-        {
-            List<Pawn> pawns = sleepy_pawn.Map.mapPawns.SpawnedPawnsInFaction(sleepy_pawn.Faction).ListFullCopy();
-            Log.Message("pawns are " + pawns.ToStringSafeEnumerable());
-            TraverseParms traverseParams = TraverseParms.For(sleepy_pawn, Danger.Deadly, TraverseMode.ByPawn, false);
-            Predicate<Pawn> surplusFinder = delegate (Pawn p)
-            {
-                int count = p.CountBeds();
-                Log.Message(p.Name + " has " + count + " beds");
-                if (((p.RaceProps.Animal || p.ownership.OwnedBed != null) && count > 0)
-                    || (count > 1))
-                {
-                    Log.Message(p.Name + " has can spare some");
-                    if (sleepy_pawn.Map.reachability.CanReach(sleepy_pawn.Position, p, PathEndMode.ClosestTouch, traverseParams))
-                    {
-                        Log.Message(sleepy_pawn.Name + " can reach " + p.Name);
-                        return true;
-                    }
-                }
-                return false;
-            };
-            List<Pawn> surplusPawns = pawns.FindAll(surplusFinder);
-            if (surplusPawns.NullOrEmpty())
-                return null;
-            Pawn generous_pawn = surplusPawns.MinBy(p => DistanceTo(p, sleepy_pawn));
-            Log.Message("surplusPawns are " + generous_pawn);
-            return generous_pawn;
-        }
+		private static Thing nearest_bed_laying_around(Pawn sleepy_pawn)
+		{
+			Predicate<Thing> validator = delegate (Thing t)
+			 {
+				 return t.GetInnerIfMinified() is Building_Bed b && b.def.building.bed_humanlike;
+			 };
+			List<Thing> groundBeds = sleepy_pawn.Map.listerThings.ThingsInGroup(ThingRequestGroup.MinifiedThing).FindAll(t => validator(t));
+			if (groundBeds.NullOrEmpty())
+				return null;
+			return groundBeds.MinBy(t => DistanceTo(t, sleepy_pawn));
+		}
 
-        private static Thing nearest_bed_laying_around(Pawn sleepy_pawn)
-        {
-            Predicate<Thing> validator = delegate (Thing t)
-             {
-                 return t.GetInnerIfMinified() is Building_Bed b && b.def.building.bed_humanlike;
-             };
-            List<Thing> groundBeds = sleepy_pawn.Map.listerThings.ThingsInGroup(ThingRequestGroup.MinifiedThing).FindAll(t => validator(t));
-            if (groundBeds.NullOrEmpty())
-                return null;
-            return groundBeds.MinBy(t => DistanceTo(t, sleepy_pawn));
-        }
-
-        private static int DistanceTo(Thing t1, Thing t2)
-        {
-            return (t1.Position - t2.Position).LengthManhattan;
-        }
-    }
+		private static int DistanceTo(Thing t1, Thing t2)
+		{
+			return (t1.Position - t2.Position).LengthManhattan;
+		}
+	}
 }
--- Source/JobDriver_PlaceBed.cs
+++ Source/JobDriver_PlaceBed.cs
@@ -8,76 +8,76 @@ using RimWorld;
 
 namespace UseBedrolls
 {
-    [DefOf]
-    public static class JobDefOf
-    {
-        public static JobDef PlaceBedroll;
-        public static JobDef TakeBedroll;
-    }
+	[DefOf]
+	public static class JobDefOf
+	{
+		public static JobDef PlaceBedroll;
+		public static JobDef TakeBedroll;
+	}
 
-    public class JobDriver_PlaceBedroll : JobDriver
-    {
-        public override bool TryMakePreToilReservations()
-        {
-            return true;
-        }
-        protected override IEnumerable<Toil> MakeNewToils()
-        {
-            job.count = 1;
-            Thing bedroll = pawn.CurJob.targetA.Thing as Thing;
+	public class JobDriver_PlaceBedroll : JobDriver
+	{
+		public override bool TryMakePreToilReservations()
+		{
+			return true;
+		}
+		protected override IEnumerable<Toil> MakeNewToils()
+		{
+			job.count = 1;
+			Thing bedroll = pawn.CurJob.targetA.Thing as Thing;
 
-            if (bedroll.Spawned)
-            {
-                Log.Message("Starting pickiup, standing at " + pawn.Position);
-                yield return Toils_Reserve.Reserve(TargetIndex.A);
-                yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.Touch);
-                yield return Toils_Haul.TakeToInventory(TargetIndex.A, 1);
-                Log.Message("Done pickup, standing at " + pawn.Position);
-            }
-            yield return Toils_Misc.TakeItemFromInventoryToCarrier(pawn, TargetIndex.A);
-            yield return Toils_Haul.CarryHauledThingToContainer();
-            yield return Toils_Construct.MakeSolidThingFromBlueprintIfNecessary(TargetIndex.B, TargetIndex.C);
-            yield return Toils_Haul.DepositHauledThingInContainer(TargetIndex.B, TargetIndex.C);
-            Toil restInBed = new Toil();
-            restInBed.initAction = delegate
-            {
-                Pawn pawn = restInBed.actor;
-                Job curJob = pawn.jobs.curJob;
-                Building_Bed bed = curJob.targetB.Thing as Building_Bed;
+			if (bedroll.Spawned)
+			{
+				Log.Message("Starting pickiup, standing at " + pawn.Position);
+				yield return Toils_Reserve.Reserve(TargetIndex.A);
+				yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.Touch);
+				yield return Toils_Haul.TakeToInventory(TargetIndex.A, 1);
+				Log.Message("Done pickup, standing at " + pawn.Position);
+			}
+			yield return Toils_Misc.TakeItemFromInventoryToCarrier(pawn, TargetIndex.A);
+			yield return Toils_Haul.CarryHauledThingToContainer();
+			yield return Toils_Construct.MakeSolidThingFromBlueprintIfNecessary(TargetIndex.B, TargetIndex.C);
+			yield return Toils_Haul.DepositHauledThingInContainer(TargetIndex.B, TargetIndex.C);
+			Toil restInBed = new Toil();
+			restInBed.initAction = delegate
+			{
+				Pawn pawn = restInBed.actor;
+				Job curJob = pawn.jobs.curJob;
+				Building_Bed bed = curJob.targetB.Thing as Building_Bed;
 
-                pawn.Map.GetComponent<PlacedBedsMapComponent>().placedBeds[pawn] = bed;
+				pawn.Map.GetComponent<PlacedBedsMapComponent>().placedBeds[pawn] = bed;
 
-                pawn.ownership.ClaimBedIfNonMedical(bed);
+				pawn.ownership.ClaimBedIfNonMedical(bed);
 
-                Job restJob = new Job(RimWorld.JobDefOf.LayDown, TargetB);
-                pawn.jobs.StartJob(restJob, JobCondition.Succeeded);
-            };
-            yield return restInBed;
-        }
-    }
+				Job restJob = new Job(RimWorld.JobDefOf.LayDown, TargetB);
+				pawn.jobs.StartJob(restJob, JobCondition.Succeeded);
+			};
+			yield return restInBed;
+		}
+	}
 
-    public class JobDriver_TakeBedroll : JobDriver_Uninstall
-    {
-        protected override int TotalNeededWork
-        {
-            get
-            {
-                return 5;
-            }
-        }
+	public class JobDriver_TakeBedroll : JobDriver_Uninstall
+	{
+		protected override int TotalNeededWork
+		{
+			get
+			{
+				return 5;
+			}
+		}
 
-        public override void Notify_Starting()
-        {
-            base.Notify_Starting();
-            if (pawn.Map.designationManager.DesignationOn(Target)?.def != DesignationDefOf.Uninstall)
-                pawn.Map.designationManager.AddDesignation(new Designation(Target, DesignationDefOf.Uninstall));
-        }
+		public override void Notify_Starting()
+		{
+			base.Notify_Starting();
+			if(pawn.Map.designationManager.DesignationOn(Target)?.def != DesignationDefOf.Uninstall)
+				pawn.Map.designationManager.AddDesignation(new Designation(Target, DesignationDefOf.Uninstall));
+		}
 
-        protected override void FinishedRemoving()
-        {
-            pawn.Map.GetComponent<PlacedBedsMapComponent>().placedBeds.Remove(pawn);
-            Thing minifiedThing = Building.Uninstall();
-            pawn.inventory.innerContainer.TryAdd(minifiedThing.SplitOff(1));
-        }
-    }
+		protected override void FinishedRemoving()
+		{
+			pawn.Map.GetComponent<PlacedBedsMapComponent>().placedBeds.Remove(pawn);
+			Thing minifiedThing = Building.Uninstall();
+			pawn.inventory.innerContainer.TryAdd(minifiedThing.SplitOff(1));
+		}
+	}
 }
