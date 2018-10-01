f0a44b8e69f3e1a8b4dc06d32db5e1dc7c266f93
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-09-30 18:58:35 -0700

    suppress DefOf warning on load

--- Source/UseBedrolls.cs
+++ Source/UseBedrolls.cs
@@ -2,6 +2,7 @@
 using Verse;
 using UnityEngine;
 using Harmony;
+using RimWorld;
 
 namespace UseBedrolls
 {
@@ -15,7 +16,18 @@ namespace UseBedrolls
 			HarmonyInstance.DEBUG = true;
 #endif
 			HarmonyInstance harmony = HarmonyInstance.Create("Uuugggg.rimworld.UseBedrolls.main");
+			
+			//Turn off DefOf warning since harmony patches trigger it.
+			harmony.Patch(AccessTools.Method(typeof(DefOfHelper), "EnsureInitializedInCtor"),
+				new HarmonyMethod(typeof(Mod), "EnsureInitializedInCtorPrefix"), null);
+			
 			harmony.PatchAll();
+		}
+
+		public static bool EnsureInitializedInCtorPrefix()
+		{
+			//No need to display this warning.
+			return false;
 		}
 
 		// public override void DoSettingsWindowContents(Rect inRect)
