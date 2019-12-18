b4b5dd87c7cb23de93f405ee76623e613b3e8fc7
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-12-18 11:31:58 -0800

    Fix RemoveAllOwners patch if bed is not spawned.

--- Source/PlacedBedsMapComponent.cs
+++ Source/PlacedBedsMapComponent.cs
@@ -26,7 +26,16 @@ namespace UseBedrolls
 	{
 		public static void Postfix(Building_Bed __instance)
 		{
-			var placedBeds = __instance.Map.GetComponent<PlacedBedsMapComponent>().placedBeds;
+			Map map = __instance.Map;
+			if (map == null) return;	//If it's not spawned, there should be no owners.
+			// If it got despawned, owners were already removed.
+			// If it hasn't spawned yet, there should be no opportunity to set owners.
+			// This can be hit when the Replace Stuff mod transfers settings from a medical bed to a new bed built over it
+			// The new bed is not yet spawned, but gets the medical setting from the old bed,
+			// and setting medical to true removes all owners
+			// That process could probably be made better by saving settings between spawned/not spawned but it hasn't been a problem elswhere.
+
+			var placedBeds = map.GetComponent<PlacedBedsMapComponent>().placedBeds;
 			foreach (KeyValuePair<Pawn, Building_Bed> kvp in placedBeds)
 				if (kvp.Value == __instance)
 				{
