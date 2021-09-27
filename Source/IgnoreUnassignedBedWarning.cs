791660e1279becd83723b59e38f9919fb806f678
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2021-09-27 12:15:45 -0700

    Ignore Unassigned Bed Warning, when on hostile maps. (new 1.3 warning message pops up when a bed is despawned, and bedrolls get despawned on hostile maps with a dumb warning)

---
+++ Source/IgnoreUnassignedBedWarning.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
+using RimWorld;
+
+namespace UseBedrolls
+{
+	[HarmonyPatch(typeof(Building_Bed), "RemoveAllOwners")]
+	class IgnoreUnassignedBedWarning
+	{
+		//private void RemoveAllOwners(bool destroyed = false)
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			//public static void Message(string text, LookTargets lookTargets, MessageTypeDef def, bool historical = true)
+			MethodInfo MessageInfo = AccessTools.Method(typeof(Messages), nameof(Messages.Message),
+				new Type[] {typeof(string), typeof(LookTargets), typeof(MessageTypeDef), typeof(bool)});
+
+			MethodInfo MaybeMessageInfo = AccessTools.Method(typeof(IgnoreUnassignedBedWarning), nameof(MaybeMessage));
+
+			foreach (var inst in instructions)
+			{
+				if(inst.Calls(MessageInfo))
+				{
+					yield return new CodeInstruction(OpCodes.Ldarg_0);//Building_Bed this
+					yield return new CodeInstruction(OpCodes.Call, MaybeMessageInfo);
+
+				}
+				else
+					yield return inst;
+			}
+		}
+
+		//public static void Message(string text, LookTargets lookTargets, MessageTypeDef def, bool historical = true)
+		public static void MaybeMessage(string text, LookTargets lookTargets, MessageTypeDef def, bool historical, Building_Bed bed)
+		{
+			//Beds on hostile maps being picked up is triggering message that you lost a bed. That's silly. Let's not.
+			//This is assuming the bed is lost due to this mod? Even if not, messaging a lost bed on an enemy map shouldn't happen
+			if (bed.Map?.ParentFaction != Faction.OfPlayer) return;
+
+			Messages.Message(text, lookTargets, def, historical);
+		}
+
+	}
+}
